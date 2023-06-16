fae42a2ba677c5566010bdc88518141b64a6547f
Fatima Jamil <jamilfatima24@gmail.com>
2023-06-16 03:08:04 -0400

    added documentation

--- TheMealDBDessertRecipesTests/Mocks/MockDataLoader.swift
+++ TheMealDBDessertRecipesTests/Mocks/MockDataLoader.swift
@@ -8,7 +8,14 @@
 import Foundation
 import XCTest
 
+/// A utility struct for loading mock data during testing to simulate network responses
 struct MockDataLoader {
+    /// Loads mock data from a JSON file in the specified bundle
+    /// - Parameters:
+    ///   - fileName: The name of the JSON file (without the file extension).
+    ///   - bundle: The bundle containing the JSON file. The main bundle is used by default.
+    /// - Returns: The loaded mock data as `Data`.
+    /// - Throws: An error if the JSON file cannot be located or if there is an error while loading the data.
     static func loadMockData(fromFile fileName: String, inBundle bundle: Bundle = Bundle.main) throws -> Data {
         guard let filePath = bundle.path(forResource: fileName, ofType: "json") else {
             throw NSError(domain: "TestError", code: 0, userInfo: [NSLocalizedDescriptionKey: "Failed to locate JSON file"])
