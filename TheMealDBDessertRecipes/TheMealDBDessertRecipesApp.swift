49be627513c5586688743a53d91e90bd37d65d10
Fatima Jamil <jamilfatima24@gmail.com>
2023-06-15 23:28:43 -0400

    Initial Commit

---
+++ TheMealDBDessertRecipes/TheMealDBDessertRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMealDBDessertRecipesApp.swift
+//  TheMealDBDessertRecipes
+//
+//  Created by Fatima Jamil on 6/15/23.
+//
+
+import SwiftUI
+
+@main
+struct TheMealDBDessertRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
